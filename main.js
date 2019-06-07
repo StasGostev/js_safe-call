0c0659a96591a51db67efc4a1dc073b9541ecb05
StasGostev <spaceomen@gmail.com>
2019-06-07 16:07:31 +0300

    'Solution_SafeCall'

---
+++ main.js
@@ -0,0 +1,12 @@
+function safeCall(func) {
+    try {
+        func();
+        return true;
+    } catch {
+        return false
+    }
+}
+safeCall(() => console.log('Hello!')); // true
+safeCall(() => JSON.parse('abc')); // false
+safeCall(() => false); // true
+safeCall(() => abc);
